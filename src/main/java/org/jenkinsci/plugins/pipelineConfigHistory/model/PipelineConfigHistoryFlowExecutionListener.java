b521d2e6e3e648b7ead289f7efedf1281be0d4c8
Jochen A. Fürbacher <Jochen-A-Fuerbacher@users.noreply.github.com>
2019-05-27 12:04:10 +0200

    Fix issue #7 (#8)
    * Get job by workflow run
    
    * Source format

--- src/main/java/org/jenkinsci/plugins/pipelineConfigHistory/model/PipelineConfigHistoryFlowExecutionListener.java
+++ src/main/java/org/jenkinsci/plugins/pipelineConfigHistory/model/PipelineConfigHistoryFlowExecutionListener.java
@@ -25,19 +25,17 @@
 package org.jenkinsci.plugins.pipelineConfigHistory.model;
 
 import hudson.Extension;
-import hudson.model.Item;
-import jenkins.model.Jenkins;
+import hudson.model.Queue.Executable;
 import org.jenkinsci.plugins.pipelineConfigHistory.PluginUtils;
 import org.jenkinsci.plugins.workflow.flow.FlowExecution;
 import org.jenkinsci.plugins.workflow.flow.FlowExecutionListener;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+import org.jenkinsci.plugins.workflow.job.WorkflowRun;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.regex.Pattern;
 import javax.annotation.Nonnull;
 
 /**
@@ -56,66 +54,41 @@ public class PipelineConfigHistoryFlowExecutionListener extends FlowExecutionLis
   public void onCompleted(@Nonnull FlowExecution execution) {
     LOG.log(Level.FINEST, "Pipeline config history triggered.");
 
-    //get the pipeline job's name.
-    Optional<String> pipelineNameOptional = getPipelineFullName(execution);
-    if (!pipelineNameOptional.isPresent()) {
-      LOG.log(Level.WARNING, "build url could not be parsed!");
-      return;
-    }
-
-    //get the corresponding Jenkins job and cast it to a pipeline job.
-
-    Item pipelineItem = Jenkins.get().getItemByFullName(pipelineNameOptional.get());
-    if (pipelineItem == null) {
-      LOG.log(Level.WARNING, "pipeline item could not be found.");
-      return;
-    } else if (!(pipelineItem instanceof WorkflowJob)) {
-      LOG.log(Level.SEVERE, "given job is a job but not a pipeline: {0}!",
-          pipelineItem.getClass());
-      return;
-    }
-    WorkflowJob workflowJob = (WorkflowJob) pipelineItem;
-
-
-    //do the I/O
-    PipelineItemHistoryDao historyDao = PluginUtils.getHistoryDao();
-
-    if (!historyDao.isHistoryPresent(workflowJob)) {
-      try {
-        historyDao.createHistory(workflowJob, getBuildNumber(execution, workflowJob));
-      } catch (IOException e) {
-        LOG.log(Level.WARNING, "pipeline config could not be created: {0}", e.getMessage());
-      }
-    } else {
-      try {
-        historyDao.updateHistory(workflowJob, getBuildNumber(execution, workflowJob));
-      } catch (IOException e) {
-        LOG.log(Level.WARNING, "pipeline config could not be updated: {0}", e.getMessage());
-      }
-    }
-  }
-
-  private Optional<String> getPipelineFullName(@Nonnull FlowExecution flowExecution) {
-    final String jobPlusSeparator = "job" + File.separator;
-
     try {
-      File flowExecutionOwner = new File(flowExecution.getOwner().getUrl());
-      //it always starts with "job/" or "job\"
-      if (!flowExecutionOwner.toString().startsWith(jobPlusSeparator)) {
-        return Optional.empty();
+      Executable executable = execution.getOwner().getExecutable();
+      if(!(executable instanceof WorkflowRun)) {
+          return;
+      }
+
+      WorkflowRun run = (WorkflowRun) executable;
+      WorkflowJob workflowJob = run.getParent();
+
+      //do the I/O
+      PipelineItemHistoryDao historyDao = PluginUtils.getHistoryDao();
+
+      if (!historyDao.isHistoryPresent(workflowJob)) {
+          createHistory(historyDao, execution, workflowJob);
       } else {
-        return Optional.of(
-            flowExecutionOwner.getParentFile()
-                .toString()
-                .replaceAll(Pattern.quote(jobPlusSeparator), "")
-                .replaceAll(Pattern.quote(File.separator), "/")
-                .replaceAll("%20", " ")
-        );
+          updateHistory(historyDao, execution, workflowJob);
       }
     } catch (IOException e) {
-      LOG.log(Level.SEVERE, "Error parsing the Pipeline Execution's URL: "
-          + e.getLocalizedMessage());
-      return Optional.empty();
+        LOG.log(Level.WARNING, "Failed to get execution: {0}", e.getMessage());
+    }
+  }
+  
+  private void createHistory(PipelineItemHistoryDao historyDao, FlowExecution execution, WorkflowJob workflowJob) {
+    try {
+      historyDao.createHistory(workflowJob, getBuildNumber(execution, workflowJob));
+    } catch (IOException e) {
+      LOG.log(Level.WARNING, "pipeline config could not be created: {0}", e.getMessage());
+    }
+  }
+  
+  private void updateHistory(PipelineItemHistoryDao historyDao, FlowExecution execution, WorkflowJob workflowJob) {
+    try {
+      historyDao.updateHistory(workflowJob, getBuildNumber(execution, workflowJob));
+    } catch (IOException e) {
+      LOG.log(Level.WARNING, "pipeline config could not be updated: {0}", e.getMessage());
     }
   }
 
