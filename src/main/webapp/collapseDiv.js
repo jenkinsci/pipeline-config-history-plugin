6e0a8837bc8a7989d9cea97757ccecb1d546212f
Robin Schulz <robin.schulz@1und1.de>
2019-08-01 13:45:51 +0200

    move switch script to file. modify collapse and the switch script so both sbs' and sl's contents' collapse statuses are synchronized.

--- src/main/webapp/collapseDiv.js
+++ src/main/webapp/collapseDiv.js
@@ -1,29 +1,82 @@
+/*
+    Doc:
+        Your elements must be named collapseButton_<YOUR_ID> and collapsableContent_<YOUR_ID>
+        For the use of single-line view and side-by-side view please start <YOUR_ID>s with  "SBS" or "SL"
+        and use integer numbers starting at 0.
+        If done so, the startup will uncollapse SBS0 and SL0 at page load.
+
+        Also, the buttons need to be of class "collapseButton" and the contents of class "collapseableContent".
+
+        TODO: implement the following: Call initCollapseDiv() once to open the first contents.
+*/
+
 var coll = document.getElementsByClassName("collapseButton");
 var contents = document.getElementsByClassName("collapseableContent");
-var i;
 
-for (i = 0; i < coll.length; i++) {
-  console.log(i + "th round");
-  var content = contents[i]
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-
-    //get number (assigned in the same order)
-    myNumber = this.id.replace('collapseButton_', '');
-    var collapseableContent = document.getElementById("collapseableContent_" + myNumber);
-
+function toggleCollapseContent(collapseButton, collapseableContent) {
     //hide/show the div and switch the button's description
-    if (collapseableContent.style.maxHeight){
-      collapseableContent.style.maxHeight = null;
-      this.innerHTML="&#x25b6;"
-    } else {
-      collapseableContent.style.maxHeight = collapseableContent.scrollHeight + "px";
-      this.innerHTML="&#x25bc;"
-    }
-  });
-
-
+        if (collapseableContent.style.maxHeight){
+          collapseableContent.style.maxHeight = null;
+          collapseButton.innerHTML="&#x25b6;"
+        } else {
+          collapseableContent.style.maxHeight = collapseableContent.scrollHeight + "px";
+          collapseButton.innerHTML="&#x25bc;"
+        }
 }
 
-//initially click first element one time to open it
-  coll[0].click();
+function getTheOtherNumber(myNumber) {
+    if (myNumber.startsWith("SBS")) {
+        return "SL" + myNumber.substring(3);
+    } else if (myNumber.startsWith("SL")) {
+        return "SBS" + myNumber.substring(2);
+    }
+}
+
+function getCollapseableContentByNumber(number) {
+    return document.getElementById("collapseableContent_" + number);
+}
+
+function getCollapseButtonByNumber(number) {
+    return document.getElementById("collapseButton_" + number);
+}
+
+function getNumberFromCollapseButton(collapseButton) {
+    return collapseButton.id.replace('collapseButton_', '');
+}
+
+
+/*
+##################################################################
+###                          Startup                          ####
+##################################################################
+*/
+//TODO wrap this in an init()-function...
+// Add the click listeners.
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+
+        myNumber = getNumberFromCollapseButton(this);
+        toggleCollapseContent(getCollapseButtonByNumber(myNumber), getCollapseableContentByNumber(myNumber));
+
+        //TODO imlement: do the same for the other element
+        if (myNumber.startsWith("SBS") || myNumber.startsWith("SL")) {
+            //toggle the other element, too
+
+            myOtherNumber = getTheOtherNumber(myNumber);
+            console.log("this number: " + myNumber + ", other number: " + myOtherNumber);
+            toggleCollapseContent(getCollapseButtonByNumber(myOtherNumber), getCollapseableContentByNumber(myOtherNumber))
+        }
+    });
+}
+
+// Initially click both first elements one time to open the contents.
+for (i = 0; i < coll.length; i++) {
+    myNumber = coll[i].id.replace('collapseButton_', '');
+    console.log("mynumber: " + myNumber);
+    if (myNumber === "SBS0") {
+    //if (myNumber === "SBS0" || myNumber === "SL0") {
+        console.log("found case:: " + myNumber);
+        coll[i].click();
+    }
+}
